03bad8d5504cd17af34ecc421d66535685de5171
Priyesh Nayak <priyeshnayak@Priyeshs-MacBook-Pro.local>
2023-07-11 13:52:33 +0530

    my app code

---
+++ app.js
@@ -0,0 +1,35 @@
+import express from 'express'
+import { getNames, getName, createName , deleteName , updateName} from './database.js'
+const app = express()
+app.use(express.json())
+app.get("/employees", async (req, res) => {
+  const name = await getNames()
+  res.send(name)
+})
+app.get("/employee/:id", async (req, res) => {
+  const id = req.params.id
+  const name = await getName(id)
+  res.send(name)
+})
+app.post("/employee", async (req, res) => {
+  const { id, name} = req.body
+  const name1 = await createName(id, name)
+  res.status(201).send(name1)
+})
+app.post("/nameDelete", async (req, res) => {
+    const { id } = req.body
+    const name = await deleteName(id)
+    res.status(201).send(name)
+  })
+app.post("/nameUpdate", async (req, res) => {
+    const { id ,name } = req.body
+    const name1 = await updateName(id,name)
+    res.status(201).send(name1)
+  })
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Error !!')
+})
+app.listen(process.env.port, () => {
+  console.log('Server is running on port 3000')
+})
---
+++ database.js
@@ -0,0 +1,37 @@
+import mysql from 'mysql2'
+import dotenv from 'dotenv'
+dotenv.config()
+const pool = mysql.createPool({
+  host: process.env.db_host,
+  user: process.env.db_user,
+  password: process.env.db_pass,
+  database: process.env.db_name
+}).promise()
+export async function getNames() {
+  const [rows] = await pool.query("SELECT * FROM employee")
+  return rows
+}
+export async function getName(id) {
+  const [rows] = await pool.query(`
+  SELECT *
+  FROM employee
+  WHERE id = ?
+  `, [id])
+  return rows[0]
+}
+export async function createName(id,name) {
+  const [result] = await pool.query(`
+  INSERT INTO employee (id, name)
+  VALUES (?, ?)` , [id, name])
+  return "inserted !!"
+}
+export async function deleteName(id) {
+    const [result] = await pool.query(`
+    DELETE from employee where id= ? ` ,[id])
+    return "Deleted !!"
+  }
+  export async function updateName(id,name) {
+    const [result] = await pool.query(`
+    Update employee set name =?  where id=?` ,[name,id])
+    return getName(id)
+  }
