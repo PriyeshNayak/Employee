a3d41659c4fce4ac873b621ff83c1ea9e5e03938
Priyesh Nayak <priyeshnayak@Priyeshs-MacBook-Pro.local>
2023-07-19 16:14:47 +0530

    added push

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import { getNames, getName, createName , deleteName , updateName} from './databa
 const app = express()
 app.use(express.json())
 app.get("/", (req, res) => {
-  res.send('Hello World !!! pull')
+  res.send('push madi khushi padi')
 })
 app.get("/employees", async (req, res) => {
   const name = await getNames()
